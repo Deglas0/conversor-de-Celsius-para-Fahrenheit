8e5a2e5ea535a8ecc9138e0d01bcee461c4db9d0
Douglas miguel da silva <124850234+Deglas0@users.noreply.github.com>
2023-05-06 01:54:33 -0300

    Update CelsiusToMain.java

--- src/CelsiusToMain.java
+++ src/CelsiusToMain.java
@@ -1,10 +1,10 @@
  public class CelsiusToMain {
 
     public static void main(String[] args) {
-        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmCelsius = 64.49;
         double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
 
-        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+        String mensagem = String.format("A temperatura de %.1f Celsius é equivalente a %.1f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
 
         System.out.println(mensagem);
 
